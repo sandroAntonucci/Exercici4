dec771b89628de5f67f79a05696e1b1c4ebdf4db
Sandro <sandro.antonucci.7e7@itb.cat>
2023-12-11 12:30:05 +0100

    Acabada la introducción de valores. Closes #1

--- Exercici4/Program.cs
+++ Exercici4/Program.cs
@@ -1,1 +1,59 @@
-﻿
+﻿using System;
+
+namespace MyApplication
+{
+
+    class CalculateAreas
+    {
+
+        static void Main()
+        {
+            const int Zero = 0;
+
+            const string MsgSquare = "Introdueix la mesura d'un costat del quadrat: ";
+            const string MsgRectangleBase = "Introdueix la base del rectangle: ";
+            const string MsgRectangleHeight = "Introdueix l'altura del rectangle: ";
+            const string MsgCircleRadius = "Introdueix el radi del cercle: ";
+            const string MsgPentagonSide = "Introdueix la mesura d'un costat del pentàgon: ";
+
+            int squareSide, rectangleBase, rectangleHeight, circleRadius, pentagonSide;
+
+            // Square Values
+            do
+            {
+                Console.WriteLine(MsgSquare);
+                squareSide = Convert.ToInt32(Console.ReadLine());
+            }while (squareSide <= Zero);
+
+            // Rectangle Values
+            do
+            {
+                Console.WriteLine(MsgRectangleBase);
+                rectangleBase = Convert.ToInt32(Console.ReadLine());
+            } while (rectangleBase <= Zero);
+
+            do
+            {
+                Console.WriteLine(MsgRectangleHeight);
+                rectangleHeight = Convert.ToInt32(Console.ReadLine());
+            } while (rectangleHeight <= Zero);
+
+            //Circle Values
+            do
+            {
+                Console.WriteLine(MsgCircleRadius);
+                circleRadius = Convert.ToInt32(Console.ReadLine());
+            } while (circleRadius <= Zero);
+
+            //Triangle Values
+            do
+            {
+                Console.WriteLine(MsgPentagonSide);
+                pentagonSide = Convert.ToInt32(Console.ReadLine());
+            } while (pentagonSide <= Zero);
+
+        }
+
+    }
+
+}
